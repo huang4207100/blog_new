2d4db2dffcc8cd354e85f395416acc1f44847de2
huang4207100 <1006190286@qq.com>
2017-12-10 15:37:36 +0800

    Finish demo app

--- Gemfile
+++ Gemfile
@@ -5,15 +5,15 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-group :productuon do
-  gem 'pg'
-  gem 'rails_12factor'
-end 
+#group :productuon do
+  #gem 'pg'
+ # gem 'rails_12factor'
+#end 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
-#gem 'sqlite3'
+gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203025548) do
+ActiveRecord::Schema.define(version: 20171210064651) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,20 @@ ActiveRecord::Schema.define(version: 20171203025548) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
